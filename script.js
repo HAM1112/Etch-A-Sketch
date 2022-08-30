23fbe1dc378fc4012643a4496b93f0fef94d4c1b
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-30 11:54:12 +0530

    added colors and eraser. new function to get random colors

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 
-let test = document.getElementById('rangebar');
-test.addEventListener('input', ()=> {console.log(test.value);} )
 
 function board(size){
     let container = document.querySelector('.container');
+    let squares = container.querySelectorAll("div")
+    squares.forEach((div) => div.remove())
     container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
     container.style.gridTempleteRows = `reapeat(${size} , 1fr)`;
 
-    //console.log(`reapeat (${size} , 1fr)`);
-
-    for (i = 0 ; i < (size ** 2); i++ ){
+    for (i = 0 ; i < (size * size); i++ ){
         let pixels = document.createElement('div')
+        pixels.addEventListener('mouseover',colorSelection)
         pixels.style.backgroundColor = "white";
-        container.insertAdjacentElement('beforeend' , pixels)
+        container.insertAdjacentElement('beforeend' , pixels) 
     }
 }
+  
+let color = 'black';  
 
-//board(16);
+function colorSelection() {
+    if (color === 'random') {
+        this.style.backgroundColor = getRandomColor();
+    }
+    else{
+        this.style.backgroundColor = color;
+    }      
+}
 
+function changeColor(choice) {
+    color = choice;
+}
 
-// function changeSize(input){
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
 
-// }
+function changeSize(input){
+    board(input);
+}
+
