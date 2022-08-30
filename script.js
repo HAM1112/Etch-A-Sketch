68e0414b0df540185a1d934bc540ec44c1f4711a
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-30 14:50:17 +0530

    changed input-type from text to range and created new div below input to show pixel size

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function board(size){
     }
 }
   
-let color = 'black';  
+let color = 'black';  //default
 
 function colorSelection() {
     if (color === 'random') {
@@ -39,15 +39,49 @@ function getRandomColor() {
     return color;
   }
   
-
+let rangeValue = document.getElementById('rangeValue').value;
 function changeSize(input){
     board(input);
+    console.log(rangeValue)
 }
 
+let btnBlack = document.getElementById('black');
+btnBlack.addEventListener('click',()=>{
+    changeColor('black');
+    btnBlack.classList.add('button-glow');
+    setTimeout(() => btnBlack.classList.remove('button-glow'), 100);
+})
 
-let clear = document.getElementById('clear')
-clear.addEventListener('click',()=>{
+let btnWhite = document.getElementById('white');
+btnWhite.addEventListener('click',()=>{
+    changeColor('white');
+    btnWhite.classList.add('button-glow');
+    setTimeout(() => btnWhite.classList.remove('button-glow'), 100);
+})
+
+let btnRandom = document.getElementById('random')
+btnRandom.addEventListener('click',()=>{
+    changeColor('random');
+    btnRandom.classList.add('button-glow');
+    setTimeout(() => btnRandom.classList.remove('button-glow'), 100);
+})
+
+let btnClear = document.getElementById('clear')
+btnClear.addEventListener('click',()=>{
     let container = document.querySelector('.container');
     let squares = container.querySelectorAll("div")
     squares.forEach((div) => div.style.backgroundColor = 'white')
+    btnClear.classList.add('button-glow');
+    setTimeout(() => btnClear.classList.remove('button-glow'), 100);
+})
+
+
+
+
+
+let slider = document.querySelector('#slider');
+let screenDisp = document.querySelector('#rangeValue');
+slider.addEventListener('input' , ()=> {
+    let val = document.getElementById('slider').value;
+    screenDisp.textContent = val;
 })
